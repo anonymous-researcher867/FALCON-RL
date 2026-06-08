89eb1753cab18db5763e01a3ab32132d07d39914
anonymous-researcher867 <abdesselam2500@gmail.com>
2026-06-08 14:07:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,21 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Federated Learning Hyperparameters
 NUM_CLIENTS = 10
-ROUNDS = 100
+ROUNDS = 500
 LOCAL_EPOCHS = 90
 BATCH_SIZE = 128
 TARGET_LAMBDA = 1e-4 
 
 # Data & Classes
-TARGET_CLASSES = sorted(["airplane", "airport", "beach"])
+TARGET_CLASSES = sorted(["airplane", "airport", "beach"]) #Due to implementation constraints associated with AutoCoRe-FL, we selected three classes from the RESISC45 dataset
+# TARGET_CLASSES = sorted(["monkshood", "snapdragon", "petunia"]) #Classes of the Flower dataset
+# TARGET_CLASSES = sorted(["tornado", "spitfire", "metroliner"]) #Classes of the Aircraft dataset
+# TARGET_CLASSES = sorted(["akiec", "bcc", "bkl", "df", "mel", "nv", "vasc"]) #Classes of the HAM10000 dataset
+
 NUM_CLASSES = len(TARGET_CLASSES)
 
 # Paths (Update these to your local/server setup)
-DATA_ROOT = "./NWPU-RESISC45"  
-CONCEPT_FILE = "./RESISC45.json" 
-FEATURE_PATH = "./results/clip_features/resisc_EVA02-CLIP-bigE-14-plus_fl_extracted_features.pt"
-MODEL_SAVE_PATH = "./results/model_weights/final_resisc_concept_model.pth"
+DATA_ROOT = "datasets/RESISC45"  
+CONCEPT_FILE = "concepts.json" 
+FEATURE_PATH = "results/RESISC45/features.pt"
+MODEL_SAVE_PATH = "results/RESISC45/weights.pth"
